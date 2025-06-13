96c3c1eb33329e6fb02b35a9c7beec3a3bea6f46
Adil Hanif <adilking0071@gmail.com>
2025-06-13 17:50:10 +0500

    Initial Commit

---
+++ Pipes/PipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PipesApp.swift
+//  Pipes
+//
+//  Created by Adil Hanif on 6/13/25.
+//
+
+import SwiftUI
+
+@main
+struct PipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
